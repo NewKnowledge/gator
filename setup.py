e8db9bb28db515715b2cb13ac64924861ed103cd
jgleason <jeffrey.gleason@newknowledge.io>
2019-08-22 14:12:26 -0400

    latest imagenet commit

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     keywords=['d3m_primitive'],
     install_requires=[
         'pandas == 0.23.4',        
-        'nk_imagenet @ git+https://github.com/NewKnowledge/imagenet.git@40deecb0c4c7645eaf4fdf2d47edea13da0fdd52#egg=nk_imagenet'
+        'nk_imagenet @ git+https://github.com/NewKnowledge/imagenet.git@6dca4c2ed4f26f4791c7e513b254a95e8ee20403#egg=nk_imagenet'
     ],
     entry_points={
         'd3m.primitives': [
