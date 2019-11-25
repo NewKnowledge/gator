75480389853ab2758a989376f339c57dcc7d2957
jgleason <jeffrey.gleason@newknowledge.io>
2019-11-25 13:34:33 -0600

    add new gator primitive, pipeline, testing script

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ setup(
     packages=['gator'],
     keywords=['d3m_primitive'],
     install_requires=[
-        'pandas == 0.23.4',        
-        'nk_imagenet @ git+https://github.com/NewKnowledge/imagenet.git@6dca4c2ed4f26f4791c7e513b254a95e8ee20403#egg=nk_imagenet'
+        'pandas>=0.23.4,<=0.25.2',    
+        'tensorflow-gpu == 2.0.0'    
+        'nk_imagenet @ git+https://github.com/NewKnowledge/imagenet.git@9f6d6aaf9e115f346cda9255b5920cf2f39ff717#egg=nk_imagenet'
     ],
     entry_points={
         'd3m.primitives': [
-            'digital_image_processing.imagenet_convolutional_neural_network.Gator = gator:gator'
+            'digital_image_processing.convolutional_neural_net.Gator = gator:gator'
         ],
     }
 )
