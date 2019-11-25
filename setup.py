9a75b03771953530d30bcfad262cc994981c6299
jgleason <jeffrey.gleason@newknowledge.io>
2019-11-25 13:38:56 -0600

    add comma in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     keywords=['d3m_primitive'],
     install_requires=[
         'pandas>=0.23.4,<=0.25.2',    
-        'tensorflow-gpu == 2.0.0'    
+        'tensorflow-gpu == 2.0.0',
         'nk_imagenet @ git+https://github.com/NewKnowledge/imagenet.git@9f6d6aaf9e115f346cda9255b5920cf2f39ff717#egg=nk_imagenet'
     ],
     entry_points={
